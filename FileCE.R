76fe7f414394a4309e1fe6c4daa2c2ce437a608a
pribadi <pribadiridwan@rocketmail.com>
2020-07-02 17:44:17 +0200

    Update

--- FileCE.R
+++ FileCE.R
@@ -1,10 +1,11 @@
-#import all necessary libraries
 #library ( tree )
 #library( ggplot2 )
+#import necessary libary
 library (caret)
 library (caTools)
 library (relaimpo)
 
+#import data set
 Boston <- read.csv("Boston.csv")
 
 #rm(Boston)
@@ -12,49 +13,55 @@ Boston <- read.csv("Boston.csv")
 #Remove unnecessary column
 #cor(Boston)
 #plot(Boston)
+#Remove index column
 Boston [ , "X" ] = list ( NULL )
-head(Boston, n = 5)
+head(Boston, n = 5) #check the first 5 row
 
-#check correlation first
+#check correlation for each variable
 cor(Boston)
 plot(Boston)
 pairs ( Boston [ c ( "chas", "nox", "rm", "dis", "ptratio", "black", "lstat") ] )
 
 #Find best model
 baseLinearModel <- lm(medv ~ . , data= Boston)
+summary(baseLinearModel)
 #better R-Square
+# - is to remove the less significant independent variable
+# + is to add """""""
 testLinearModel <- lm(medv ~ . - indus - age - tax - zn - rad - crim, data = Boston)
 #--------------#
 linearModel <- lm(medv ~ lstat + chas + ptratio + dis + zn + nox + black
                   + rm  + rad, data = Boston)
 summary(testLinearModel)
 #interaction model
+#Abhisek as to fabio
 interactionModel <- lm(medv ~ I(lstat ^ 2)+ rm + tax + lstat, data= Boston)
 summary(interactionModel)
 
 # Relative importance of variables
-testModel <- calc.relimp (testLinearModel, type = "lmg" )
-testModel
-sum ( testModel$lmg )
+interactionModel <- calc.relimp (interactionModel, type = "lmg" )
+sum ( interactionModel$lmg )
 
-barplot ( sort ( testModel$lmg, decreasing = TRUE ), 
+barplot ( sort ( interactionModel$lmg, decreasing = TRUE ), 
           col = c ( 2:10 ), main = "Relative Importance of Predictors", 
           xlab = "Predictor Labels", ylab = "Shapley Value Regression", 
           font.lab = 2 )
 
-#Create prediction test
 
+#Create prediction test
 #1. set split ratio
 set.seed(100)
-splitData = sample.split(Boston$medv, SplitRatio = 0.75)
+splitData <- sample.split(Boston$medv, SplitRatio = 0.75)
 View(splitData)
+
 #2. Set train data
-dataForTrain = subset(Boston, splitData == TRUE)
-dataForTest = subset(Boston, splitData == FALSE)
+dataForTrain <- subset(Boston, splitData == TRUE)
+dataForTest <- subset(Boston, splitData == FALSE)
 
 #4. Check error for test data
+#Root mean square E
 sqrt (mean ( ( dataForTest$medv - predict.lm ( interactionModel, dataForTest ) ) ^ 2 ))
 
 #check using RMSE
-predictionsModel = predict ( interactionModel, dataForTest)
+predictionsModel <- predict ( interactionModel, dataForTest)
 RMSE (predictionsModel, dataForTest$medv)
--- chasCluster.R
+++ chasCluster.R
@@ -1,7 +1,8 @@
 Boston <- read.csv("Boston.csv")
 
 #Scaling data for KNN
-scaledData <- scale(Boston)
+#scaledData <- scale(Boston)
+library( ggplot2 )
 library(corrplot)
 library(caTools)
 library(gridExtra)
@@ -31,7 +32,8 @@ points(clusteredData$centers, pch=3, cex=2)
 ### unreliable value ###
 
 #Cluster with Binary Classification
-Boston$chas <- as.factor(Boston$chas)
+Boston$chas <- as.factor(Boston$chas) 
+#To find how many 0 / 1 we have for chas
 summary(Boston)
 
 #Split data to train
@@ -42,15 +44,17 @@ testDataSet = subset(Boston, splitDataSet == FALSE)
 
 #visualize data set
 #View(trainDataSet)
+#-4  is to exclude fourth column
 cormat <- cor(trainDataSet[,-4])
 corrplot(cormat,method='number')
 # Find high correlation variable
-# Nox & rad | rad & tax | age & rad
+# Nox & dis | rad & tax | age & rad
 p1 <- ggplot(data = trainDataSet,aes(nox,rad,col=chas))+geom_point(alpha=0.5)
 p2 <- ggplot(data = trainDataSet,aes(tax,rad,col=chas))+geom_point(alpha=0.5)
 p3 <- ggplot(data = trainDataSet,aes(age,rad,col=chas))+geom_point(alpha=0.5)
 grid.arrange(p1,p2,p3,nrow=3)
 #check against chas
+#box plot
 p1<-ggplot(data = trainDataSet,aes(x = chas,y = crim,fill=chas))+geom_boxplot()
 p2<-ggplot(data = trainDataSet,aes(x = chas,y = zn,fill=chas))+geom_boxplot()
 p3<-ggplot(data = trainDataSet,aes(x = chas,y = indus,fill=chas))+geom_boxplot()
@@ -70,9 +74,9 @@ grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,nrow=3)
 lrm <- glm ( chas ~ rm + dis + indus + lstat, data = trainDataSet, family = binomial )
 lrm <- glm ( chas ~ . - crim - zn - black, data = trainDataSet, family = binomial )
 summary(lrm)
-predictTest = predict ( lrm, type = "response", newdata = testDataSet )
+predictTest <- predict ( lrm, type = "response", newdata = testDataSet )
 
-table ( testDataSet$chas, predictTest > 0.5 )
+table ( testDataSet$chas, predictTest > 0.1 )
 
 # Baseline accuracy
 1 - mean ( Boston$chas ) 
@@ -83,21 +87,21 @@ ROCRperf = performance ( ROCRpred, "tpr", "fpr" )
 plot ( ROCRperf, colorize = TRUE, print.cutoffs.at = seq ( 0, 1, by = 0.1 ), text.adj = c ( -0.2, 1.7 ) )
 
 # Test for clustering #
-hc.complete = hclust ( dist ( trainDataSet ), method = "complete" )
-hc.average = hclust ( dist ( trainDataSet ), method = "average" )
-hc.single = hclust ( dist ( trainDataSet ), method = "single" )
+hc.complete <- hclust ( dist ( trainDataSet ), method = "complete" )
+hc.average <- hclust ( dist ( trainDataSet ), method = "average" )
+hc.single <- hclust ( dist ( trainDataSet ), method = "single" )
 
 par ( mfrow = c ( 1, 3 ) )
-plot ( hc.complete , main = " Complete Linkage ", xlab= "", sub = "", cex = .9 )
+plot ( hc.complete , main = " Complete Linkage ", xlab= "", sub = "" )
 plot ( hc.average , main = " Average Linkage ", xlab= "", sub = "", cex = .9 )
 plot ( hc.single , main =" Single Linkage ", xlab= "", sub ="", cex = .9 )
 
 ##scaling
 xsc = scale ( trainDataSet )
-par ( mfrow = c ( 1, 1 ) )
+par ( mfrow <- c ( 1, 1 ) )
 plot ( hclust ( dist ( xsc ), method = "single" ), main = "Hierarchical Clustering with Scaled Features")
 
 #predict
-treePart = rpart ( chas ~ . - crim - zn - black , data = trainDataSet )
+treePart = rpart ( chas ~ . - crim - zn - black - medv , data = trainDataSet )
 treePart = rpart ( chas ~ rm + dis + indus + lstat , data = trainDataSet )
 rpart.plot ( treePart )
